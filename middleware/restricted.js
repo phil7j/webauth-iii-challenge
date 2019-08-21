27f8876f1e3a4e1a4a5f23e806a60201f1f99944
Philip Johnson <phil7j@gmail.com>
2019-08-21 15:06:25 -0700

    fixed issue where token wasn't being read in middleware

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -3,12 +3,13 @@ const jwt = require('jsonwebtoken')
 const secrets = require('../config/secrets.js')
 
 module.exports = function restricted(req, res, next) {
-  const token = req.headers.authorization;
-
+  console.log(req.headers)
+  const token = req.headers.token;
   if(token) {
     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
       if(err){
         // token is invalid
+        console.log(err)
         res.status(401).json({message: 'you shall not pass, invalid token'})
       } else {
         // good token
